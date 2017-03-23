8165dab7c0405f96d1b7a843ac59db1ef0a9f1ba
Gregory KLEIN <gregoryklein@free.fr>
2017-03-23 08:58:36 +0100

    Added output style option

--- index.js
+++ index.js
@@ -1,44 +1,47 @@
 'use strict';
 
 var nodeSass = require('node-sass'),
-    path = require('path'),
-    fs = require('fs'),
-    through = require('through2'),
-    cheerio = require('cheerio');
+  path = require('path'),
+  fs = require('fs'),
+  through = require('through2'),
+  cheerio = require('cheerio');
 
 var PLUGIN_NAME = 'gulp-polymer-sass';
 
-var gulpPolymerScss = function gulpPolymerScss() {
-    return through.obj(function(file, enc, cb) {
+var gulpPolymerScss = function gulpPolymerScss(config) {
+  config = config || {};
+  return through.obj(function (file, enc, cb) {
 
-        var $ = cheerio.load(file.contents.toString());
-        var el = $('style[lang="scss"]');
+    var $ = cheerio.load(file.contents.toString());
+    var el = $('style[lang="scss"]');
 
-        if(el) {
-            var scss = el.text();
+    if(el) {
+      var scss = el.text();
 
-            if (!scss) {
-                return cb(null,file);
-            }
+      if (!scss) {
+        return cb(null,file);
+      }
 
-            nodeSass.render({
-                data: scss.toString(),
-                outputStyle: 'compressed'
-            }, function (err, compiledScss) {
+      var outputStyle = config.outputStyle || 'nested'; // nested, expanded, compact, compressed
 
-                if (err || !compiledScss) {
-                    console.log('Error compiling scss: ' + err);
-                    return cb();
-                }
+      nodeSass.render({
+        data: scss.toString(),
+        outputStyle: outputStyle
+      }, function (err, compiledScss) {
 
-                el.text(compiledScss.css.toString());
-                el.removeAttr('lang');
-
-                file.contents = new Buffer($.html(), 'utf8');
-                return cb(null,file);
-            });
+        if (err || !compiledScss) {
+          console.log('Error compiling scss: ' + err);
+          return cb();
         }
-    });
-}
+
+        el.text(compiledScss.css.toString());
+        el.removeAttr('lang');
+
+        file.contents = new Buffer($.html(), 'utf8');
+        return cb(null,file);
+      });
+    }
+  }.bind(config));
+};
 
 module.exports = gulpPolymerScss;
